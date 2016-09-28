994a39e418cdeead93982f95ea8ea020c548dd38
Melissa Ly <melissakly@berkeley.edu>
2016-09-28 16:57:29 -0700

    hw 2 first submission

--- app/controllers/concerns/foobar.rb
+++ app/controllers/concerns/foobar.rb
@@ -1,5 +1,13 @@
 class Foobar
   # ENTER CODE FOR Q2 HERE
+  attr_accessor :name, :baz
+
   def initialize(param)
+  	@name = param 
   end
+
+  def bar (first, second, third)
+  	puts "Your result"
+  	puts "#{:first}#{:param}{:third}"
+  	end
 end
